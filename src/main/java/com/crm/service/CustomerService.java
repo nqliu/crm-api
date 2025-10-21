8179d9478a4a11990900442197db3d1f2e6c9c9c
nqliu <2765126643@qq.com>
2025-10-21 18:20:48 +0800

    feat:客户管理 & 公海管理

--- src/main/java/com/crm/service/CustomerService.java
+++ src/main/java/com/crm/service/CustomerService.java
@@ -1,7 +1,17 @@
 package com.crm.service;
 
+import com.crm.common.result.PageResult;
 import com.crm.entity.Customer;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.crm.entity.Department;
+import com.crm.query.CustomerQuery;
+import com.crm.query.IdQuery;
+import com.crm.vo.CustomerVO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.net.http.HttpResponse;
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +22,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2025-10-12
  */
 public interface CustomerService extends IService<Customer> {
+
+    PageResult<CustomerVO> getPage(CustomerQuery query);
+    void exportCustomer(CustomerQuery query, HttpServletResponse httpResponse);
+    void saveOrUpdate(CustomerVO  customerVO);
+    void removeCustomer(List<Integer> ids);
+    void customerToPublicPool(IdQuery idQuery);
+    void publicPoolToPrivate(IdQuery idQuery);
 
 }
