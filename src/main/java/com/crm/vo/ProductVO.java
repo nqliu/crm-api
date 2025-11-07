39e6d8b24a2ff4203275f3ea2fddb6b15d4eb7b5
nqliu <2765126643@qq.com>
2025-11-08 07:57:57 +0800

    feat:合同管理

---
+++ src/main/java/com/crm/vo/ProductVO.java
@@ -0,0 +1,17 @@
+package com.crm.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class ProductVO {
+    @JsonProperty("pId")
+    private Integer id;
+    @JsonProperty("pName")
+    private String pName;
+    @JsonProperty("totalPrice")
+    private BigDecimal totalPrice;
+    private Integer count;
+    private  BigDecimal price;
+}
--- target/generated-sources/annotations/com/crm/convert/CustomerConvertImpl.java
+++ target/generated-sources/annotations/com/crm/convert/CustomerConvertImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-26T10:34:19+0800",
+    date = "2025-11-02T11:38:03+0800",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
 )
 public class CustomerConvertImpl implements CustomerConvert {
